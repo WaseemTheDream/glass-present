20f05fa8e32eaf996dc8047435cdaf9bcf7e485a
Bonnie <bonnie.eisenman@gmail.com>
2013-09-14 09:15:19 -0700

    Added Slide.java file to represent slide data. For Gson.

---
+++ GlassViewer/GlassViewer/src/main/java/com/clarity/glassviewer/Slide.java
@@ -0,0 +1,37 @@
+package com.clarity.glassviewer;
+
+/**
+ * Created by bonnie on 9/14/13.
+ */
+
+public class Slide {
+
+    private String speaker_notes;
+    private String page_id;
+    private String img_url;
+
+    public String getSpeaker_notes() {
+        return speaker_notes;
+    }
+
+    public void setSpeaker_notes(String speaker_notes) {
+        this.speaker_notes = speaker_notes;
+    }
+
+    public String getPage_id() {
+        return page_id;
+    }
+
+    public void setPage_id(String page_id) {
+        this.page_id = page_id;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+}
